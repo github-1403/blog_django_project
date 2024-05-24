b3fb01cc247edb91984b026b405e8feed179d622
github-1403 <fllstck.shp@gmail.com>
2024-05-24 16:11:04 +0330

    create posts_list template and render it in post_list view (in this view first extract all posts from db and then send all post to post_list template)

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
 def post_list_view(request):
-    return HttpResponse('posts_list')
+    posts_list = Post.objects.all()
+    context = {'all_posts': posts_list}
+    return render(request, template_name='blog/posts_list.html', context=context)
+
 
