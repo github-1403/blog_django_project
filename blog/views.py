a46f2b4f66b86c9395d1a218f5537497ffbd447b
github-1403 <fllstck.shp@gmail.com>
2024-05-30 14:56:43 +0330

    add post create view and url , and create NewPost form

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.contrib.auth.models import User
+from .forms import NewPostForm
+from django.shortcuts import redirect, reverse
 
 
 def post_list_view(request):
@@ -12,4 +15,16 @@ def post_detail_view(request, pk):
     post = get_object_or_404(Post, pk=pk)
     context = {"post": post}
     return render(request, template_name='blog/post_detail.html', context=context)
+
+
+def post_create_view(request):
+    if request.method == "POST":
+        form = NewPostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to=reverse('posts_list'))
+    else:
+        form = NewPostForm()
+
+    return render(request, 'blog/post_create.html', context={'form': form})
 
